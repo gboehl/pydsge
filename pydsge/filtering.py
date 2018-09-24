c0214b2bee5d7ef1b05304622de105063ad83450
Gregor Boehl <admin@gregorboehl.com>
2018-09-24 21:10:06 +0200

    ll based on shocks

--- pydsge/filtering.py
+++ pydsge/filtering.py
@@ -27,11 +27,40 @@ def create_filter(self, alpha = .25, scale_obs = 0.):
 
     self.ukf    = ukf
 
+"""
 def get_ll(self):
 
     self.ll     = self.ukf.batch_filter(self.Z)[2]
 
     return self.ll
+"""
+
+def get_ll(self, use_rts=True, info=False):
+
+    from scipy.stats import norm 
+
+    if info == 1:
+        st  = time.time()
+
+    X1, cov, _  = self.ukf.batch_filter(self.Z)
+
+    if use_rts:
+        X1, _, _    = self.ukf.rts_smoother(X1, cov)
+
+    exo     = nl.pinv(self.SIG)
+    stds    = np.diag(self.QQ(self.par))
+
+    ll  = 0
+    for i in range(X1.shape[0]-1):
+        eps     = X1[i+1] - self.t_func(X1[i])[0]
+        ll      += np.sum(norm.logpdf(exo @ eps, scale=stds))
+
+    if info == 1:
+        print('Filtering done in '+str(np.round(time.time()-st,3))+'seconds.')
+
+    self.ll     = ll
+
+    return ll
 
 
 def run_filter(self, use_rts=True, info=False):
