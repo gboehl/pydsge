3d182de4712b3c2f4623f2db8719718c6347f278
gboehl <admin@gregorboehl.com>
2019-02-20 14:27:40 +0100

    fix error in obs_cov

--- pydsge/filtering.py
+++ pydsge/filtering.py
@@ -14,8 +14,7 @@ def create_obs_cov(self, scale_obs=0.1):
     if not hasattr(self, 'Z'):
         warnings.warn('No time series of observables provided')
     else:
-        # sig_obs = np.var(self.Z, axis=0)*scale_obs**2
-        sig_obs = np.var(self.Z, axis=0)*scale_obs
+        sig_obs = np.var(self.Z, axis=0)*scale_obs**2
 
         self.obs_cov = np.diagflat(sig_obs)
 
