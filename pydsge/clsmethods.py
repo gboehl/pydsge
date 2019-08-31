91d2c84f2020cb8d59d961726024d457c4986010
Gregor Boehl <admin@gregorboehl.com>
2019-08-31 21:47:24 +0200

    automated input data handling

--- pydsge/clsmethods.py
+++ pydsge/clsmethods.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
+import pandas as pd
 from .parser import DSGE
 from .stats import summary
 from .engine import preprocess
@@ -123,33 +124,53 @@ def info_m(self, **args):
     else:
         tune = self.fdict['tune']
 
+    if hasattr(self, 'name'):
+        name = self.name
+    else:
+        name = self.fdict['name']
+
+    if hasattr(self, 'description'):
+        description = self.description
+    else:
+        description = self.fdict['description']
+
     cshp = self.get_chain().shape
 
-    info_str = 'last %s of %s samples in %s chains with %s parameters.' %(cshp[0] - tune, cshp[0], cshp[1], cshp[2])
+    info_str = 'Title: %s (description: %s). Last %s of %s samples in %s chains with %s parameters.' %(name, description, cshp[0] - tune, cshp[0], cshp[1], cshp[2])
 
     return info_str
 
+def get_data(self, csv_file, sep=None, start=None, end=None):
 
-## old stuff, left to be integrated
-"""
-def chain_masker(self):
-    iss = np.zeros(len(self.prior_names), dtype=bool)
-    for v in self.prior_names:
-        iss = iss | (self.prior_names == v)
-    return iss
+    if csv_file[-4:] != '.csv':
+        raise TypeError('data format must be `.csv`.')
 
-def means(self):
-    x = self.par_fix
+    d0 = pd.read_csv(csv_file, sep=sep).dropna()
 
-    x[self.prior_arg] = chain[self.tune:].mean(axis=(0, 1))
-    return list(x)
+    for o in self['observables']:
+        if str(o) not in d0.keys():
+            raise KeyError('%s is not in the data!' %o)
 
-def medians(self):
-    x = self.par_fix
-    x[self.prior_arg] = np.median(self.chain[self.tune:], axis=(0, 1))
-    return list(x)
+    dates = pd.date_range(str(int(d0['year'][0])), periods=d0.shape[0], freq='Q')
 
-"""
+    d0.index = dates
+
+    self.obs = [str(o) for o in self['observables']]
+    d1 = d0[self.obs]
+
+    if start is not None:
+        start = str(start)
+
+    if end is not None:
+        end = str(end)
+
+    d2 = d1.loc[start:end]
+
+    self.data = d2
+    self.fdict['data'] = d2
+    self.fdict['obs'] = self.obs
+
+    return d2
 
 DSGE.t_func = t_func
 DSGE.set_path = set_path
@@ -180,3 +201,25 @@ DSGE.traceplot = traceplot_m
 DSGE.posteriorplot = posteriorplot_m
 DSGE.summary = summary_m
 DSGE.info = info_m
+DSGE.get_data = get_data
+
+## old stuff, left to be integrated
+"""
+def chain_masker(self):
+    iss = np.zeros(len(self.prior_names), dtype=bool)
+    for v in self.prior_names:
+        iss = iss | (self.prior_names == v)
+    return iss
+
+def means(self):
+    x = self.par_fix
+
+    x[self.prior_arg] = chain[self.tune:].mean(axis=(0, 1))
+    return list(x)
+
+def medians(self):
+    x = self.par_fix
+    x[self.prior_arg] = np.median(self.chain[self.tune:], axis=(0, 1))
+    return list(x)
+
+"""
