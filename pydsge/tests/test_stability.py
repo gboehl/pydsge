f8ea1e29bf2167bd5483ef5475c885088aa01e41
pcschreiber1 <82368796+pcschreiber1@users.noreply.github.com>
2021-11-29 11:34:01 +0100

    Implementation of test_stability but still relying on toy pickles.
    Also, several TODOs left.

--- pydsge/tests/test_stability.py
+++ pydsge/tests/test_stability.py
@@ -4,26 +4,34 @@
 import pickle
 import pytest
 from pydsge import * # imports eg. the DSGE class and the examples
+import numpy as np # For np.all()
+import __main__
 
 # Load output under new commit
-from resources.getting_started import * #TODO: difference test will fail at the moment
+# from resources.getting_started import * #TODO: difference test will fail at the moment
+from resources.toy_getting_started import * #TODO: delete
 
-@pytest.fixture("new_output")
-def get_output_newcommit():
+
+def is_picklable(obj): #TODO: import function from file where getting_started.py is pickled (currently getting it from toy_getting_started)
+    """
+    check if obj can be dumped to a .pkl file
+    """
+    try:
+        pickle.dumps(obj)
+    except Exception:
+        return False
+    return True
+
+__main__.is_picklable = is_picklable #add is_picklable to the envirnoment for pickle load (from https://www.py4u.net/discuss/246734)
+
+
+
+@pytest.fixture(scope="module")
+def new_output(global_vars = dir()): #need to call dir() outside of function,  otherwise only finds local objects
     #TODO: create docstring
-    def is_picklable(obj): #TODO: import function from file whether getting_started.py is pickled
-        """
-        check if obj can be dumped to a .pkl file
-        """
-        try:
-            pickle.dumps(obj)
-        except Exception:
-            return False
-        return True
-
     ## Get relevant variables in environment
     bk_new = {}
-    for k in dir():
+    for k in global_vars:
         obj = globals()[k]
         if not k.startswith('_') and is_picklable(obj):
             try:
@@ -33,10 +41,11 @@ def get_output_newcommit():
 
     return bk_new
 
-@pytest.fixture("stable_output")
-def get_output_stable():
+@pytest.fixture(scope="module")
+def stable_output():
     #TODO: create docstring
-    with open('resources/toy_example.pkl', 'rb') as f: #TODO: need to change the pickled file that is imported
+    print(os.getcwd())
+    with open('pydsge/tests/resources/toy_example.pkl', 'rb') as f: #TODO: need to change the pickled file that is imported
         bk_restore = pickle.load(f)
 
     return bk_restore
@@ -47,7 +56,9 @@ def test_what_output_is_there(new_output, stable_output):
     '''
     # Get difference between the two dictionaries
     diff = (set(stable_output.keys()) - set(new_output.keys())) | (set(new_output.keys()) - set(stable_output.keys()))
-    assert diff == {"bk", "bk_new", "bk_restore"} #[order of loading will influence the difference]
+    print(diff)
+
+    assert diff == {"bk", "In", "Out"} #[order of loading will influence the difference]
 
 def test_content_of_outputs(new_output, stable_output):
     #TODO: create docstring
@@ -55,11 +66,12 @@ def test_content_of_outputs(new_output, stable_output):
     diff = (set(stable_output.keys()) - set(new_output.keys())) | (set(new_output.keys()) - set(stable_output.keys()))
 
     # Get collection of shared variables to loop over
-    shared_vars = (set(bk_restore.keys()) | set(bk_new.keys())) - diff - {"In"} #TODO: here getting manually rid of "In", but should do that elsewhere
+    error_vars = {"In", "example_model", "example_data", "meta_data", "pth", "example", "data_file", "yaml_file", "chain", "res_dict"} #TODO: main issue is that there is a difference from where they are called, once local, once from package
+    shared_vars = (set(stable_output.keys()) | set(new_output.keys())) - diff - error_vars #TODO: here getting manually rid of "In", but should do that elsewhere
 
     # Loop over shared vars
     for key in shared_vars:
-        if type(bk_new[key]).__name__ == "DataFrame": #Do we really need separate test for DataFrames -> would np.all() not work just as well?
-            assert bk_new[key].equals(bk_restore[key]), f"Error with {key}"
+        if type(new_output[key]).__name__ == "DataFrame": #Do we really need separate test for DataFrames -> would np.all() not work just as well?
+            assert new_output[key].equals(stable_output[key]), f"Error with {key}"
         else:
-            assert np.all(bk_new[key] == bk_restore[key]), f"Error with {key}" #Use np.all() for arrays
+            assert np.all(new_output[key] == stable_output[key]), f"Error with {key}" #Use np.all() for arrays
