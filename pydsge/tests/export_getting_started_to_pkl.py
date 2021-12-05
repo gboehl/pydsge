ee367d965bc0111a6100cb3defb3facb3e09f13c
pcschreiber1 <82368796+pcschreiber1@users.noreply.github.com>
2021-12-05 20:41:13 +0100

    Put exec() into function using exec(code, d)

--- pydsge/tests/export_getting_started_to_pkl.py
+++ pydsge/tests/export_getting_started_to_pkl.py
@@ -37,19 +37,19 @@ def _is_picklable(obj):
     return True
 
 
-def _get_global_vars(global_vars):
-    """Get variables from globals by names of variables.
+def filter_pickable(global_vars):
+    """Filter the variables that are pickable
 
     Args:
         global_vars (array-like): The names of variables to get
     
     Returns:
-        dict: Dictionary containing names of variables and variables
+        dict: Dictionary containing names of objects and their values
     
     """
     bk = {}
     for k in global_vars:
-        obj = globals()[k]
+        obj = global_vars[k]
         if _is_picklable(obj):
             try:
                 bk.update({k: obj})
@@ -58,7 +58,7 @@ def _get_global_vars(global_vars):
     return bk
 
 
-def backup_notebook_global_vars(path):
+def notebook_to_pickable_dict(path):
     """Excute jupyter notebook and then save variables defined in notebook.
     This function converts notebook to python code and then excutes the code.
     Finally it put all public variables that defined in notebook into dictionary 
@@ -72,17 +72,16 @@ def backup_notebook_global_vars(path):
     bk : :dict
         Dictionary containing names of variables and variables that defined in notebook.
     """
+    # Step 1: Convert notebook to script
     code = _nbconvert_python(path)
     code = code.replace("get_ipython()", "# get_ipython()")
 
-    _var_before_exec = dir()
-    exec(code)
+    # Step 2: Execute script and save variables in dictionary
+    d= {}
+    exec(code, d)
 
-    _global_vars = set(dir()) - set(_var_before_exec)
-    _global_vars = list(filter(lambda k : not k.startswith('_'), _global_vars))
-    print(f"global_vars:{_global_vars}")
-
-    bk = _get_global_vars(_global_vars)
+    # Step 3: Filter for pickable variables
+    bk = filter_pickable(d)
     return bk
 
 
@@ -101,17 +100,7 @@ def save_to_pkl(path, obj):
 # excute jupyter notebook and save global variables
 notebook_path = "docs\\getting_started.ipynb"
 
-code = _nbconvert_python(notebook_path)
-code = code.replace("get_ipython()", "# get_ipython()")
-
-_var_before_exec = dir()
-exec(code)
-
-_global_vars = set(dir()) - set(_var_before_exec)
-_global_vars = list(filter(lambda k : not k.startswith('_'), _global_vars))
-print(f"global_vars:{_global_vars}")
-
-bk = _get_global_vars(_global_vars)
+bk = notebook_to_pickable_dict(notebook_path)
 
 # to save session
 save_path = "pydsge\\tests\\resources\\getting_started_stable.pkl"
