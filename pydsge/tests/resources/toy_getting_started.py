46ed693cdab842bba5162511d4c0a618385b15d4
pcschreiber1 <82368796+pcschreiber1@users.noreply.github.com>
2021-12-06 13:57:24 +0100

    Cleaning up test folder.

--- pydsge/tests/resources/toy_getting_started.py
+++
@@ -1,9 +0,0 @@
-from pydsge import *
-import pickle
-
-#Creating the New version with deliberate difference
-yaml_file, data_file = example
-mod = DSGE.read(yaml_file)
-par = mod.set_par('calib')
-shock_list = ('e_u', 4.0, 0) # (name, size, period) ## size 3 instead of 4!!
-X1, (L1, K1), _ = mod.irfs(shock_list, verbose=True)
