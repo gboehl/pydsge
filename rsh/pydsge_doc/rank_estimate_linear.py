b4d770f83d9a57aac2f98fb797a99b4969aa59aa
YuxinWang2020 <yuxinwang9712@gmail.com>
2022-01-13 17:33:49 +0800

    fix multiprocessing error about spawn in windows

--- rsh/pydsge_doc/rank_estimate_linear.py
+++ rsh/pydsge_doc/rank_estimate_linear.py
@@ -5,40 +5,53 @@ import numpy as np
 import pandas as pd
 from pydsge import DSGE
 import emcee
+from sys import platform
+from multiprocessing import freeze_support
 
-yaml = 'rsh/pydsge_doc/rank.yaml'
+if __name__ == '__main__':
+    freeze_support()
 
-mod = DSGE.read(yaml)  
+    yaml = 'rsh/pydsge_doc/rank.yaml'
 
-mod.name = 'rank_test_linear'
-mod.description = 'RANK, linear estimation'
+    mod = DSGE.read(yaml)  
 
-mod.path = 'rsh/bs0/npz'
+    mod.name = 'rank_test_linear'
+    mod.description = 'RANK, linear estimation'
 
-d0 = pd.read_csv('rsh/pydsge_doc/data.csv', sep=';', index_col='date', parse_dates=True).dropna() 
+    mod.path = 'rsh/bs0/npz'
 
-mod.load_data(d0, start='1983', end='2008Q4')
+    mod.debug = False
 
-mod.prep_estim(linear=True, seed=0, verbose=True)
+    d0 = pd.read_csv('rsh/pydsge_doc/data.csv', sep=';', index_col='date', parse_dates=True).dropna() 
 
-mod.filter.R = mod.create_obs_cov(1e-1)
-ind = mod.observables.index('FFR')
-mod.filter.R[ind,ind] /= 1e1 
+    mod.load_data(d0, start='1983', end='2008Q4')
 
-fmax = None
+    mod.prep_estim(linear=True, seed=0, verbose=True)
 
-moves = [(emcee.moves.DEMove(), 0.8), 
-         (emcee.moves.DESnookerMove(), 0.2),]
+    mod.filter.R = mod.create_obs_cov(1e-1)
+    ind = mod.observables.index('FFR')
+    mod.filter.R[ind,ind] /= 1e1 
 
-p0 = mod.tmcmc(200, 200, 0, fmax, moves=moves, update_freq=100, lprob_seed='set')
-mod.save()
+    fmax = None
 
-mod.mcmc(p0, moves=moves, nsteps=2000, tune=500, update_freq=500, lprob_seed='set', append=True)
-mod.save()
+    moves = [(emcee.moves.DEMove(), 0.8), 
+            (emcee.moves.DESnookerMove(), 0.2),]
 
-pars = mod.get_par('posterior', nsamples=250, full=True)
-epsd0 = mod.extract(pars, nsamples=1)
-mod.save_rdict(epsd0)
+    p0 = mod.tmcmc(200, 200, 0, fmax, moves=moves, update_freq=100, lprob_seed='set')
+    mod.save()
 
-mod.mode_summary()
-mod.mcmc_summary()
+    mod.mcmc(p0, moves=moves, nsteps=200, tune=500, update_freq=500, lprob_seed='set', append=True)
+    mod.save()
+
+    pars = mod.get_par('posterior', nsamples=250, full=True)
+    epsd0 = mod.extract(pars, nsamples=1)
+    mod.save_rdict(epsd0)
+
+    mod.mode_summary()
+    mod.mcmc_summary()
+
+
+    print(f"platform: {platform}")
+    print(f"debug: {mod.debug}")
+else:
+    print(__name__)
