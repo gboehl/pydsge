1359fc681278ee57aba7fd0f6207e953d0146ecb
Gregor Boehl <admin@gregorboehl.com>
2022-07-08 23:40:03 +0200

    fix dependencies

--- setup.py
+++ setup.py
@@ -41,8 +41,8 @@ setup(
         "cloudpickle",
         "h5py",
         "emcwrap>=0.1.3",
-        "grgrlib>=0.1.12",
-        "econsieve>=0.0.5",
+        "grgrlib>=0.1.13",
+        "econsieve>=0.0.56,
     ],
     include_package_data=True,
 )
